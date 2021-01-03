3aaa7afdcd0e407c86ebc0fc8e465baecbd61061
Bhavika2006 <70019267+Bhavika2006@users.noreply.github.com>
2021-01-03 22:45:12 +0530

    Add files via upload

--- division.js
+++ division.js
@@ -15,8 +15,6 @@ class Division{
      fill("white")
      rect(pos.x,pos.y,this.width,this.height)
 
-    for( var k = 0; k<=width ; k = k+80){
-        division.push(new Division(k,height/2,10,200))
-    }
+    
  }
 }
--- particle.js
+++ particle.js
@@ -1,22 +1,18 @@
 class Particle{
     constructor(x,y,radius){
-        var options = {
-        isStatic:true
-        }
+
     
-  this.body = Bodies.cirlce(x,y,radius,options);
+  this.body = Bodies.circle(x,y,radius);
   this.radius = radius
-  this.color = color(random(0,225))
+  this.color = color(random(0,225),random(0,225),random(0,225))
  
   World.add(world,this.body)
  }
  display(){
      var pos = this.body.position
-     ellipseMode(CENTER)
-     
+     ellipseMode(RADIUS)
+     fill(this.color)
      ellipse(pos.x,pos.y,this.radius)
-     if(frameCount%60 ===0){
-         particle.push(new Particle(random(width/2-10,width/2 +10),-10,20))
-     }
+    
  }
 }
